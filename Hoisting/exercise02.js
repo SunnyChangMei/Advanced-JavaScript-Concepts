e83f05e9232f685e4b2b895b592f9292d210c8ea
Sunny Mei <mkaleader@gmail.com>
2019-03-09 12:34:58 -0500

    add scopechain

--- Hoisting/exercise02.js
+++ Hoisting/exercise02.js
@@ -1,6 +1,19 @@
-function bigBrother(){
-debugger;
+//* littleBrother is already defined
+//* littleBrother(it is me) gets hoisted => undefined
+//* bigBrother() invoked last littleBrother() return 'no me!'
 
+/**
+ * how this function executed
+ * 1. console.log(bigBrother()); 
+ * 2. call bigBrother (this: global)
+ * 3. line 18 executed return littleBrother();
+ * 4. line 21 return value: 'no me!'
+ * 5. back to 18 executed return littleBrother();
+ * ! 6. littleBrother is already defined = 'no me!'
+ * ! 7. done and exit the function
+ */
+// debugger;
+function bigBrother(){
   function littleBrother() {
     return 'it is me!';
   }
@@ -9,7 +22,6 @@ debugger;
     return 'no me!';
   }
 }
-
 // Before running this code, what do you think the output is?
 console.log(bigBrother());
 //! print out no me! why?
---
+++ scope/scopechain.js
@@ -0,0 +1,32 @@
+// Scope:
+function sayMyName() {
+  var a = 'a';
+  return function findName() {
+    var b = 'b';
+    // console.log(c) //! C ReferenceError: c is not defined, done and exit function. printName() never called. If console.log(c) is removed printName will call and return 'Andrei Neagoie'.
+    return function printName() {
+      var c = 'c';
+      return 'Andrei Neagoie'
+    }
+  }
+}
+
+sayMyName()()() //! need to add three ()()() call each function. else it just return the function name with out any output.
+
+function findName() {
+  var b = 'b';
+  return printName();
+}
+
+function printName() {
+  var c = 'c'
+  return 'Andrei Neagoie'
+}
+
+function sayMyName() {
+  var a = 'a';
+  return findName()
+}
+
+sayMyName()
+//! print out 'Andrei Neagoie'
